4ce7ad05759a471db04a4732de1bb16f43ede45d
satpanha <panha.sat618@gmail.com>
2026-02-27 12:29:27 +0700

    Homework/ Injection Provider

---
+++ main_production.dart
@@ -0,0 +1,16 @@
+import 'package:provider/provider.dart';
+
+import 'data/repositories/songs/song_repository_remote.dart';
+import 'data/repositories/songs/song_repository.dart';
+import 'main_common.dart';
+
+/// Configure provider dependencies for production environment
+List<Provider> get providersRemote {
+  return [
+    Provider<SongRepository>(create: (context) => SongRepositoryRemote()),
+  ];
+}
+
+void main() {
+  mainCommon(providersRemote);
+}
